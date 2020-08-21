57100583adea86ab5ab7b2f7b87c6fdc3d7490b7
aa-ag <aaron.aguerrevere@gmail.com>
2020-08-21 17:12:46 -0500

    northernmost point in the US

--- Google Maps/script.js
+++ Google Maps/script.js
@@ -1,15 +1,24 @@
-let map;
+let southMost;
+let northMost;
 
 document.addEventListener("DOMContentLoaded", ()=> {
     let s = document.createElement("script");
     document.head.appendChild(s);
     s.addEventListener("load", () => {
-        map = new google.maps.Map(document.getElementById("map"), {
+        southMost = new google.maps.Map(document.getElementById("map"), {
             center: {
                 lat: 24.5465,
                 lng: -81.7975
             },
             zoom: 21,
+            mapTypeId: 'satellite'
+        });
+        northMost = new google.maps.Map(document.getElementById("map-north"), {
+            center: {
+                lat: 71.2905556,
+                lng: -156.7886111
+            },
+            zoom: 15,
             mapTypeId: 'satellite'
         });
     });
