9adacf7dee4361a321fa64c792800a8502945972
aa-ag <aaron.aguerrevere@gmail.com>
2020-09-05 19:31:19 -0500

    drawing X

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,9 +1,24 @@
+const X_class = 'x';
+const Circle_class = 'circle';
+
 const cellElements = document.querySelectorAll('[data-cell');
+let circleTurn;
 
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick,  {once: true})
 })
 
 function handleClick(e) {
-    console.log('clicked!')
+    const cell = e.target;
+    const currentClass = circleTurn ? Circle_class : X_class;
+    // placeMark
+    placeMark(cell, currentClass)
+
+    // check for Win
+    // check for Draw
+    // Switch turns
 };
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
