8dd597aeacd321e2062c2b8c86806464a445c5c4
aa-ag <aaron.aguerrevere@gmail.com>
2020-09-05 20:08:48 -0500

    final

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -15,16 +15,23 @@ const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 const winningMessageTextElement = document.querySelector('[data-winning-message]')
 const winningMessageElement = document.getElementById('winmes')
+const restartButton =  document.getElementById('restart')
 let circleTurn;
 
 startGame()
 
+restartButton.addEventListener('click', startGame)
+
 function startGame() {
     circleTurn = false;
     cellElements.forEach(cell => {
+        cell.classList.remove(X_class)
+        cell.classList.remove(Circle_class)
+        cell.removeEventListener('click', handleClick)
         cell.addEventListener('click', handleClick,  {once: true})
     })
     setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
 }   
 
 function handleClick(e) {
@@ -36,20 +43,28 @@ function handleClick(e) {
     // check for Win
     if (checkWin(currentClass)) {
         endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurns()
+        setBoardHoverClass()
     }
-    // check for Draw
-    // Switch turns
-    swapTurns()
-    setBoardHoverClass()
 };
 
 function endGame(draw) {
     if (draw) {
-
+        winningMessageTextElement.innerText = "Draw!"
     } else {
         winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
     }
     winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_class) ||
+        cell.classList.contains(Circle_class)
+    })
 }
 
 function placeMark(cell, currentClass) {
@@ -77,3 +92,4 @@ function checkWin(currentClass) {
         })
     })
 }
+
